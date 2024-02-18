dcf3192944ec16ba40e69686e992b2f76d86dcef
hiroyuki.abe.nu <abeh9197@gmail.com>
2024-02-18 23:56:44 +0900

    useState

--- useState.tsx
+++ useState.tsx
@@ -1,10 +1,20 @@
+/* We should never directly update state. Ex: color = "red" is not allowed. */
+
 import { useState } from "react";
 import { ReactDOM } from "react-dom/client";
 
 function FavoriteColor() {
-    const [color , setColor] = useState("Red");
+    const [color, setColor] = useState("Red");
 
-    return <h1>My favorite color {color}!</h1>
+    return (
+        <>
+            <h1>My favorite color is {color}!</h1>
+            <button
+                type="button"
+                onClick={() => setColor("blue")}
+            >Blue</button>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
