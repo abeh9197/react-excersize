fea29a6eb7d782a52cb07409201b08ff71f88614
hiroyuki.abe.nu <abeh9197@gmail.com>
2024-02-18 23:49:03 +0900

    edit useState

--- useState.tsx
+++ useState.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { ReactDOM } from "react";
+import { ReactDOM } from "react-dom/client";
 
 function FavoriteColor() {
-    const [color, setColor] = useState("Red");
+    const [color , setColor] = useState("Red");
 
-    return <h1>My favorite color is {color}.</h1>
+    return <h1>My favorite color {color}!</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FavoriteColor />)
+root.render(<FavoriteColor />);
